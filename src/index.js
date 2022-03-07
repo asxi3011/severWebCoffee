0b9225e3fb53b0e40001297cdce95e82a10d457f
asxi3011 <asxi3011@gmail.com>
2022-03-07 19:46:48 +0700

    fix loi err R10

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
 const path = require('path');
-const port = process.env.PORT; //heoroku port
+const port = process.env.PORT || 3030; //heoroku port
 const route = require('./route/index.js')
 const methodOverride = require('method-override');
 var cookieParser = require('cookie-parser')
@@ -87,6 +87,6 @@ app.engine('.hbs', exphbs(
 app.use(methodOverride('_method'));
 app.set('view engine', '.hbs');
 route(app);
-server.listen(3030, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:3030`)
 })
