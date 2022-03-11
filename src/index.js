40fcfe0f7079aad8288dbfec9044b3a1470b7d6a
asxi3011 <asxi3011@gmail.com>
2022-03-11 21:31:05 +0700

    set Header

--- src/index.js
+++ src/index.js
@@ -8,11 +8,6 @@ var cookieParser = require('cookie-parser')
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' })
 const app = express();
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
 app.set('views',(path.join(__dirname,'resources/views')));
