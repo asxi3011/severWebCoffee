22cdd8f31cbaeaebf9a9e99118bc97ed58814b96
asxi3011 <asxi3011@gmail.com>
2022-03-11 21:33:19 +0700

    set Header

--- src/resources/validate/api.cors.js
+++ src/resources/validate/api.cors.js
@@ -1,5 +1,4 @@
 module.exports.setHeader =function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     next();
   };
