86c78e1cb79cdae52e8ddc4ed18d3d8987484516
asxi3011 <asxi3011@gmail.com>
2022-03-21 21:59:29 +0700

    Sua file CORS

--- src/route/home.js
+++ src/route/home.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const cors = require("../resources/validate/api.cors");
+
 const home = require('../app/controller/homeController');
 
             //==== HIEU =======
--- src/route/index.js
+++ src/route/index.js
@@ -2,10 +2,11 @@
 const home = require('./home')
 const manager = require('./manager')
 const me = require('./me')
+const cors = require("../resources/validate/api.cors");
 function route(app){
     app.use('/manager', manager);
     app.use('/me', me);
-    app.use('/', home);
+    app.use('/',cors.setHeader, home);
   
 }
 
